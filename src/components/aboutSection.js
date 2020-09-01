7387756bd52d5d78094aa8a6596d772616a84b0d
Sergio <solivares@xpaso.com>
2020-09-01 17:34:51 -0400

    map and color added

---
+++ src/components/aboutSection.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+const AboutUsSection = () => {
+  return (
+    <>
+      <h1>About Us</h1>
+    </>
+  )
+}
+
+export default AboutUsSection
--- src/components/servicesSection.js
+++ src/components/servicesSection.js
@@ -4,41 +4,58 @@ const Services = () => {
   return (
     <div style={{ margin: `50px 0` }}>
       <h1 style={{ textAlign: `center` }}>Services</h1>
-      <div style={{ display: `flex`, flexDirection: `row` }}>
+      <div style={{ display: `flex`, flexDirection: `row`, flexWrap: `wrap` }}>
+        <div className="service-box">
+          <h2>Mounting</h2>
+          <ul>
+            <li>TV mounting with hidden cables.</li>
+            <li>Water Heater.</li>
+            <li>Air duct mounting.</li>
+          </ul>
+        </div>
         <div className="service-box">
           <h2>Painting</h2>
           <ul>
-            <li>
-              Resurfacing and refinishing
-              <li>3D Wall</li>
-            </li>
+            <li>Resurfacing and refinishing.</li>
+            <li>3D Wall.</li>
           </ul>
         </div>
         <div className="service-box">
           <h2>Remodeling</h2>
           <h3>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
-            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-            aliquip ex ea commodo consequat.
+            <ul>
+              <li>Swimming pool.</li>
+              <li>Bathrooms</li>
+              <li>Kitchens</li>
+            </ul>
           </h3>
         </div>
         <div className="service-box">
           <h2>Cleaning</h2>
           <ul>
             <li>Carpet cleaning.</li>
-            <li>Streching</li>
-            <li>Repairs carpet.</li>
+            <li>Streching.</li>
             <li>Water damange.</li>
+            <li>Air duct cleaning.</li>
           </ul>
         </div>
         <div className="service-box">
           <h2>Reparing</h2>
           <h3>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
-            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-            aliquip ex ea commodo consequat.
+            <ul>
+              <li>Sinks and toilets.</li>
+              <li>Repairs Carpet</li>
+            </ul>
+          </h3>
+        </div>
+        <div className="service-box">
+          <h2>Construction</h2>
+          <h3>
+            <ul>
+              <li>Deck Design.</li>
+              <li>Swimming Pool.</li>
+              <li>A/C and Heater system.</li>
+            </ul>
           </h3>
         </div>
       </div>
